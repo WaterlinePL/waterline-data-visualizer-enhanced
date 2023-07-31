45ce4a7ce5dc417a9787852f0e2bddc791c1b188
wojdzie <wojdzi@outlook.com>
2023-07-31 16:17:15 +0200

    Finished User Interface with dialog buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Container/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+<script setup lang="ts">
+import Container from "./components/Container.vue";
 </script>
 
-<style lang="scss">
-
+<style>
+@import '~primeicons/primeicons.css';
 @import "~primevue/resources/primevue.min.css";
 @import "~primevue/resources/themes/md-light-deeppurple/theme.css";
 
@@ -26,6 +17,47 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font: inherit;
+  vertical-align: baseline;
+}
+
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
+import Menubar from 'primevue/menubar'
+import Panel from 'primevue/panel'
+import Calendar from 'primevue/calendar'
+import ProgressBar from "primevue/progressbar";
+import Chart from "primevue/chart";
+import Divider from "primevue/divider";
+import InputNumber from 'primevue/inputnumber';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Dialog from 'primevue/dialog';
 
 // create new Vue app
 const app = createApp(App)
@@ -11,6 +21,16 @@ app.use(PrimeVue)
 
 // register components
 app.component('Button', Button)
+app.component('Menubar', Menubar)
+app.component('Panel', Panel)
+app.component('Calendar', Calendar)
+app.component('ProgressBar', ProgressBar)
+app.component('Chart', Chart)
+app.component('Divider', Divider)
+app.component('InputNumber', InputNumber)
+app.component('TabView', TabView)
+app.component('TabPanel', TabPanel)
+app.component('Dialog', Dialog)
 
 // mount the app
 app.mount("#app");
