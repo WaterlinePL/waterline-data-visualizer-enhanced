a7237b16d0f8a6cad1b87e1428c23d5ab1b0d5b3
wojdzie <wojdzi@outlook.com>
2023-09-05 19:38:20 +0200

    Stations loading is changed

--- src/state/stations.state.js
+++ src/state/stations.state.js
@@ -1,20 +1,36 @@
 import { defineStore } from 'pinia';
-import pointStationsData from '../data/stations/stations_point.json'
-import lineStationsData from '../data/stations/stations_line.json'
-import polygonStationsData from '../data/stations/stations_polygon.json'
+
+function importStations() {
+    const data = [];
+    const dataContext = require.context('../data/stations', true, /\.json$/);
+    dataContext.keys().forEach((key) => {
+        const formattedKey = key.replace('./', '');
+        const module = import((`../data/stations/${formattedKey}`));
+        data.push(module);
+    });
+    return Promise.all(data);
+}
 
 export const useStationsStore = defineStore('stations', {
     state: () => ({
-        stations: [pointStationsData, lineStationsData, polygonStationsData],
+        stations: [],
         stationsMap: new Map(),
     }),
     actions: {
-        initialize() {
+        async initialize() {
             if (this.stationsMap.size === 0) {
-                this.stations.flat()
-                    .forEach(station => {
-                    this.stationsMap.set(station.id, station);
-                });
+                try {
+                    const stationDataModules = await importStations();
+                    stationDataModules.forEach((module) => {
+                        const stationData = module.default;
+                        this.stations.push(stationData);
+                        stationData.forEach((station) => {
+                            this.stationsMap.set(station.id, station);
+                        });
+                    });
+                } catch (error) {
+                    console.error('Error loading data files:', error);
+                }
             }
         },
         uploadStationsFile(stations) {
